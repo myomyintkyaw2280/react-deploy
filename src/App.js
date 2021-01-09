2bf82b70be5009d0f4f8696e14004fe8ec323ecf
myomyintkyaw2280 <myomyintkyaw2280@gmail.com>
2021-01-10 01:06:23 +0630

    deploy my react app to git hub pages

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import profile from './img/profile.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My React App to Git Hub live</h1>
+      <img src={profile} alt="Profile Image" />
+    </div>
+  );
+}
+
+export default App;
